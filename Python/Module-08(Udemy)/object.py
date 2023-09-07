a45b8d6e18a31bd750907f9fe7b655035099d105
Rahul Rudra <rahulrudra665@gmail.com>
2023-09-07 08:50:37 +0600

    new python commmit

--- Python/Module-08(Udemy)/object.py
+++ Python/Module-08(Udemy)/object.py
@@ -13,6 +13,23 @@ class Cat():
         self.breed=breed
         self.name=name
         pass
-    
+    def meow(self,number):
+        self.number=number
+        print("Meow"+self.number) 
+        print("Meow"+self.number) 
 my_cat=Cat(name='Rahul',breed='Boy')
 print(my_cat.name)
+print(my_cat.meow(number='19'))
+my_cat.meow(number='12')
+
+
+class Circle:
+    pi=3.1416
+    def __init__(self,radius) -> None:
+        self.radius=radius
+        pass
+    def get_circumference(self):
+        return self.pi*self.radius*2
+    
+my_circle=Circle(12)
+print(my_circle.get_circumference())
